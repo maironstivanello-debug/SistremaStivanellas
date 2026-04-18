1dff43c0395ef481511b1c7022041c11da4a81ea
Mairon Stivanello <seuemail@gmail.com>
2026-04-18 19:51:32 -0300

    terminei o sistema

---
+++ nbproject/SistemaStivanello.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemastivanello;
+
+/**
+ *
+ * @author u09278438111
+ */
+public class SistemaStivanello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
